b7eab7dfecd1955cdb3a79200e18f1cb88326e77
Yatharth Patil <140743526+YatharthPatil14@users.noreply.github.com>
2024-08-29 11:10:49 +0530

    Create Template.cpp
    My main Template

---
+++ Template.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+const char nl = '\n';
+const int MOD = 1e9 + 7;
+
+void fn() {}
+
+int main() {
+    // cout << fixed << setprecision(10);
+    cin.tie(nullptr)->sync_with_stdio(false);
+    int tt; cin >> tt;
+    while (tt--)
+        { fn(); }
+}
