1705831c0e0090dc1638d3c95f4cd955e9ae116b
Yatharth Patil <140743526+YatharthPatil14@users.noreply.github.com>
2024-08-29 11:17:48 +0530

    Create BInaryExponentiation.cpp
    A class for binary exponentiation

---
+++ BInaryExponentiation.cpp
@@ -0,0 +1,13 @@
+class BinExp {
+    int pow(int base, int power) {
+        if (base == 1 || power == 0) {return 1;}
+        if (base == 0) {return 0;}
+        ll result = 1;
+        while(power > 0) {
+            if (power & 1) {(result *= base) %= MOD;}
+            power >>= 1;
+            base = (base * base) % MOD;
+        }
+        return result;
+    }
+};
