849d72fdedf578f98bbc1fdb2fea105f931628ab
Yatharth Patil <140743526+YatharthPatil14@users.noreply.github.com>
2024-09-06 19:49:38 +0530

    Update PnC.cpp

--- PnC.cpp
+++ PnC.cpp
@@ -3,12 +3,12 @@ int fact[N + 1];
 int fact_inv[N + 1];
 
 int pow(int a, int b) {
-    ll out = 1, x = a;
+    int out = 1;
     while(b > 0) {
         if (b & 1) 
-            { (out *= x) %= MOD; }
+            { (out *= a) %= MOD; }
         b >>= 1;
-        (x *= x) %= MOD;
+        (a *= a) %= MOD;
     }
     return out;
 }
@@ -16,15 +16,12 @@ int pow(int a, int b) {
 void pre() {
     fact[0] = fact_inv[0] = fact [1] = fact_inv[1] = 1;
     for (int i = 2; i <= N; ++i) {
-        fact[i] = (1LL * fact[i-1] * i) % MOD;
+        fact[i] = (fact[i-1] * i) % MOD;
         fact_inv[i] = pow(fact[i], MOD - 2);
     }
 }
 
 int C(int n, int r) {
     if (r > n) return 0;
-    ll t = fact[n];
-    t = (t * fact_inv[n - r]) % MOD;
-    t = (t * fact_inv[r]) % MOD;
-    return t;
+    return (((fact[n] * fact_inv[n - r]) % MOD) * fact_inv[r]) % MOD;
 }
